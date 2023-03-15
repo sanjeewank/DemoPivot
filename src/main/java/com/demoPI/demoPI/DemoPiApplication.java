08e9eb2fcd535540466aeb513ec9ec46e7a6e7e4
sanjeewank <sanjeewankulathunga@gmail.com>
2023-03-16 00:44:50 +0100

    project init

---
+++ src/main/java/com/demoPI/demoPI/DemoPiApplication.java
@@ -0,0 +1,13 @@
+package com.demoPI.demoPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPiApplication.class, args);
+	}
+
+}
